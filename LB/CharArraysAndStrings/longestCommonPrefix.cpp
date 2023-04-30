98c8f2191882104a06a9380fbb931066b899606b
Harshit Yadav <harshityadav47@gmail.com>
2023-05-01 02:54:55 +0530

    longest prefix sum done!

--- LB/CharArraysAndStrings/longestCommonPrefix.cpp
+++ LB/CharArraysAndStrings/longestCommonPrefix.cpp
@@ -3,13 +3,26 @@
 using namespace std;
 
 string longCommPrefix(vector<string> strs){
-    int minLen = INT_MAX;
-    for(int i=0;i<strs.size();i++){
-        if(strs[i].size()<minLen){
-            minLen = strs[i].size();
+        int minLen = INT_MAX;
+        for(int i=0;i<strs.size();i++){
+            if(strs[i].size()<minLen){
+                minLen = strs[i].size();
+            }
         }
-    }
-    string res;
+        string res = "";
+        char pre = strs[0][0];
+        int x = 0;
+        for(int i=0;i<minLen;i++){
+            for(auto str: strs){
+                if(str[x]!=pre){
+                    return res;
+                }
+            }
+            x++;
+            res.push_back(pre);
+            pre = strs[0][x];
+        }
+        return res;
 }
 
 int main(){
